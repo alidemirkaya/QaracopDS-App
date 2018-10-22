dde98ba8a71b19a11397b684c51958634adc2d51
Ali Demirkaya <alidmrky@gmail.com>
2018-10-22 21:23:47 +0300

    Yeni Class

---
+++ QaracopDS App/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QaracopDS_App
+{
+    class Class1
+    {
+        public void Gonder()
+        {
+            Form1 fr = new Form1();
+            fr.Show();
+        }
+    }
+}
